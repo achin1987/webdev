631fe22820cdacfbbc57b871672912cf3667c32c
achin1987 <achin.madot@gmail.com>
2019-06-21 17:32:44 +0800

    adding new todo. The click listeners not working for new todos added.

--- jQuery/ToDoListProject/Assets/js/todo.js
+++ jQuery/ToDoListProject/Assets/js/todo.js
@@ -4,9 +4,19 @@ $("li").click(function() {
 });
 
 //click on X to delete todo
-$("span").click(function() {
+$("span").click(function(event) {
     $(this).parent().fadeOut(500, function() {
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+    if (event.which === 13) {
+        //grabbing new todo text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        //create a new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
 });
