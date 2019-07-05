3c80649ebadb54fa57995da48658d5547c969e41
achin1987 <achin.madot@gmail.com>
2019-07-05 23:09:30 +0800

    Add a better header/title, make campgrounds display in grid

--- Back_End/YelpCamp/app.js
+++ Back_End/YelpCamp/app.js
@@ -10,6 +10,12 @@ app.use(bodyParser.urlencoded({extended:true}));
 var campgrounds = [
     {name: "Salmon creek", image: "https://pixabay.com/get/57e2d54b4852ad14f6da8c7dda793f7f1636dfe2564c704c732d79d09544c35a_340.jpg"},
     {name: "Granite hill", image: "https://pixabay.com/get/57e1d14a4e52ae14f6da8c7dda793f7f1636dfe2564c704c732d79d09544c35a_340.jpg"},
+    {name: "mountain goats", image: "https://pixabay.com/get/54e6d0434957a514f6da8c7dda793f7f1636dfe2564c704c732d79d09544c35a_340.jpg"},
+    {name: "Salmon creek", image: "https://pixabay.com/get/57e2d54b4852ad14f6da8c7dda793f7f1636dfe2564c704c732d79d09544c35a_340.jpg"},
+    {name: "Granite hill", image: "https://pixabay.com/get/57e1d14a4e52ae14f6da8c7dda793f7f1636dfe2564c704c732d79d09544c35a_340.jpg"},
+    {name: "mountain goats", image: "https://pixabay.com/get/54e6d0434957a514f6da8c7dda793f7f1636dfe2564c704c732d79d09544c35a_340.jpg"},
+    {name: "Salmon creek", image: "https://pixabay.com/get/57e2d54b4852ad14f6da8c7dda793f7f1636dfe2564c704c732d79d09544c35a_340.jpg"},
+    {name: "Granite hill", image: "https://pixabay.com/get/57e1d14a4e52ae14f6da8c7dda793f7f1636dfe2564c704c732d79d09544c35a_340.jpg"},
     {name: "mountain goats", image: "https://pixabay.com/get/54e6d0434957a514f6da8c7dda793f7f1636dfe2564c704c732d79d09544c35a_340.jpg"}
 ];
 
