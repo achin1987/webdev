b2387cdfe3b14a4066c51cef8259bf892a003cd9
achin1987 <achin.madot@gmail.com>
2019-07-11 12:38:58 +0800

    EDIT and UPDATE: Add EDIT route Add UPDATE route

--- Back_End/RESTful/app.js
+++ Back_End/RESTful/app.js
@@ -72,6 +72,29 @@ app.get("/blogs/:id", (req, res) =>{
     });
 });
 
+//EDIT ROUTE
+app.get("/blogs/:id/edit", (req, res) =>{
+    Blog.findById(req.params.id, (err, foundBlog) =>{
+        if(err){
+            res.redirect("/blogs");
+        } else{
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+    
+});
+
+//UPDATE ROUTE
+app.put("/blogs/:id", (req, res) =>{
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) =>{
+        if(err){
+            res.redirect("/blogs");
+        } else{
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+
 app.listen(3000, () =>{
     console.log("Server connected");
 });
