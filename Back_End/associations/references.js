0828ed6bcbf5029fe7c8b50ee8ece1c81e9797d4
achin1987 <achin.madot@gmail.com>
2019-07-17 22:42:58 +0800

    intro to module.export,  move models into seperate files

--- Back_End/associations/references.js
+++ Back_End/associations/references.js
@@ -2,58 +2,42 @@ var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/blog_demo_2", { useNewUrlParser: true });
 mongoose.set('useFindAndModify', false);
 
-//POST - TITLE , CONTENT
-var postSchema = new mongoose.Schema({
-    title: String,
-    content: String
-});
-var Post = mongoose.model("Post", postSchema);
+var Post = require("./models/post");
 
-//USER, EMAIL, NAME
-var userSchema = new mongoose.Schema({
-    email: String,
-    name: String,
-    posts: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Post"
-        }
-    ]
-});
-var User = mongoose.model("User", userSchema);
+var User = require("./models/user");
 
 // User.create({
 //     email: "bob@gmail.com",
 //     name: "Bob Fisher"
 // });
 
-// Post.create({
-//     title: "How to cook best pasta pt.3",
-//     content: "da da da da da da"
-// }, (err, post) =>{
-//     User.findOne({email: "bob@gmail.com"}, (err, foundUser)=>{
-//         if(err){
-//             console.log(err);
-//         } else{
-//             foundUser.posts.push(post);
-//             foundUser.save((err, data)=>{
-//                 if(err){
-//                     console.log(err);
-//                 } else{
-//                     console.log(data);
-//                 }
-//             });
-//         }
-//     });
-// });
+Post.create({
+    title: "How to cook best pasta pt.4",
+    content: "bababababbabababa"
+}, (err, post) =>{
+    User.findOne({email: "bob@gmail.com"}, (err, foundUser)=>{
+        if(err){
+            console.log(err);
+        } else{
+            foundUser.posts.push(post);
+            foundUser.save((err, data)=>{
+                if(err){
+                    console.log(err);
+                } else{
+                    console.log(data);
+                }
+            });
+        }
+    });
+});
 
 //find the user
 //find all posts for that user
 
-User.findOne({email: "bob@gmail.com"}).populate("posts").exec((err, user)=>{
-    if(err){
-        console.log(err);
-    } else{
-        console.log(user);
-    }
-});
+// User.findOne({email: "bob@gmail.com"}).populate("posts").exec((err, user)=>{
+//     if(err){
+//         console.log(err);
+//     } else{
+//         console.log(user);
+//     }
+// });
