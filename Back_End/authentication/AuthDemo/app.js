413c2fc6fc4e9e4e6cf8d3d0574d9eed5c16bd92
achin1987 <achin.madot@gmail.com>
2019-07-22 22:27:22 +0800

    Authentication: create user model, configure passport

--- Back_End/authentication/AuthDemo/app.js
+++ Back_End/authentication/AuthDemo/app.js
@@ -3,14 +3,27 @@ var bodyParser              = require("body-parser");
 var mongoose                = require("mongoose");
 var passport                = require("passport");
 var passportLocal           = require("passport-local");
-var passportLocalMongoose   = require("passport-local-mongoose");
 var expressSession          = require("express-session");
+var User                    = require("./models/user");
+var passportLocalMongoose   = require("passport-local-mongoose");
 
 //APP CONFIG
 var app = express();
 app.set("view engine", "ejs");
 mongoose.connect("mongodb://localhost:27017/AuthDemo", {useNewUrlParser: true});
 mongoose.set("useFindAndModify", false);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(expressSession({
+    secret: "What is your pet name",
+    resave: false, 
+    saveUninitialized: false
+}));
+
+//these methods read the session and takes data from the session and encode/un-encode it
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.get("/", (req, res) =>{
