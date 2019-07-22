a4da268b5c8b3c4adacdbb9565b4da1d685154ca
achin1987 <achin.madot@gmail.com>
2019-07-22 22:52:56 +0800

    Authentication: Add register routes, Add register form, Add sign up link to the home page

--- Back_End/authentication/AuthDemo/app.js
+++ Back_End/authentication/AuthDemo/app.js
@@ -10,6 +10,7 @@ var passportLocalMongoose   = require("passport-local-mongoose");
 //APP CONFIG
 var app = express();
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 mongoose.connect("mongodb://localhost:27017/AuthDemo", {useNewUrlParser: true});
 mongoose.set("useFindAndModify", false);
 app.use(passport.initialize());
@@ -25,13 +26,36 @@ app.use(expressSession({
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
+//INDEX ROUTE
 app.get("/", (req, res) =>{
     res.render("home");
 });
 
+
+
 app.get("/secret", (req, res) =>{
     res.render("secret");
+});
+
+//AUTH ROUTES
+//SHOW SIGN UP FORM
+app.get("/register", (req, res) =>{
+    res.render("register");
+});
+
+//handling user sign up
+app.post("/register", (req, res) =>{
+    
+    User.register(new User({username: req.body.username}), req.body.password, (err, user) =>{
+        if(err){
+            console.log(err);
+            return res.render("register");
+        } else{
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/secret");
+            });
+        }
+    });
 });
 
 app.listen(3000, () =>{
